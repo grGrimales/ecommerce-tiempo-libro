16fa6c626b4b2fbfc1d8574fc1451632f02ab7f4
GrGrimales <grojasgrimales@gmail.com>
2022-03-20 10:37:35 -0500

    Se crea components para menu se aplica estilos con sass adaptado a dispositivos mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from "./logo.svg";
-import "./App.css";
+
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Tiempo libro</h1>
+
+      <div>
+        loreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff v
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+        oreffffffffffffffffffffffffffffffffffffffffff+ ffffffffffffffffffffff
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <header className="header">
+        <h2 className="header__title">Tiempo Libro</h2>
+        <nav className="header__navegation">
+          <div className="header__icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className="nav" className={click ? "nav active" : "nav"}>
+            <li className="nav__item">
+              <a className="nav__links" href="#">
+                Inicio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a className="nav__links" href="#">
+                {" "}
+                Libros
+              </a>
+
+              {/* <ul>
+                <li>Inicio</li>
+                <li>Inicio</li>
+                <li>Inicio</li>
+              </ul> */}
+            </li>
+
+            <li className="nav__item">
+              <a className="nav__links" href="#">
+                {" "}
+                Nosotros
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a className="nav__links" href="#">
+                {" "}
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
