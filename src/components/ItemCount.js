9cb84edd493387959729d94c2066eabc00dae8b8
GrGrimales <grojasgrimales@gmail.com>
2022-03-29 17:47:24 -0500

    Se crea nuevo componente contador que te permite agregar o quitar un elemento

---
+++ src/components/ItemCount.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+
+export const ItemCount = ({ initial, stock, onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    console.log(count);
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const increment = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    } else {
+      console.log(`Solo contamos con ${stock} unidades disponibles`);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `Solo contamos con ${stock} unidades disponibles`,
+      });
+    }
+  };
+
+  return (
+    <div className="containerAdd">
+      <p className="containerAdd__p">{count}</p>
+      <div className="containerAdd__btn">
+        <button
+          type="submit"
+          className="containerAdd__btnDecrement"
+          onClick={decrement}
+        >
+          -
+        </button>
+
+        <button
+          type="submit"
+          className="containerAdd__btnAdd"
+          onClick={() => onAdd(count)}
+        >
+          Agregar al carrito
+        </button>
+
+        <button
+          type="submit"
+          className="containerAdd__btnIncrement"
+          onClick={increment}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { ItemCount } from "./ItemCount";
 
 export const ItemListContainer = ({ greetings }) => {
+  const handleOnAdd = (quantity) => {
+    console.log(`se agregaron ${quantity} productos al carrito`);
+  };
+
   return (
     <div>
       <h1 className="textCenter">{greetings}</h1>
+
+      <ItemCount initial={1} stock={15} onAdd={handleOnAdd} />
+      <ItemCount initial={1} stock={10} onAdd={handleOnAdd} />
+
+      <ItemCount initial={1} stock={5} onAdd={handleOnAdd} />
     </div>
   );
 };
